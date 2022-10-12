7a118c79ba7bcbfc0b92e6dacb4e2429b88996ec
UphillD <13258958+UphillD@users.noreply.github.com>
2022-10-12 19:55:17 +0300

    Add module-specific code

---
+++ common/CO.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# Praetorian H2020 Project
+# Work Package 6	:	Response Coordination
+# Task 4			:	Integration with Social Media
+# ~~~~~~~~~~~~~~~~~~~~
+# CO-specific functionality
+
+import re
+
+tag_rules = 'lexicon_CO'
+tag_identifiers = ''
+tag_tweets = 'CO_tweets'
+a = 2
+b = 3
+
+def cleanup_text(text):
+	text = text.replace('Disclaimer: This tweet contains false information.', '')
+	text = re.sub(r'@\S+', '', text)            # Remove mentions
+	text = re.sub(r'http\S+', '<link>', text)   # Remove URLs
+	text = re.sub(r'[\W]', ' ', text)           # Remove symbols
+	text = " ".join(text.split())               # Remove duplicate spaces
+	text = text.strip()                         # Remove heading or trailing spaces
+	return(text)
+
+# Classify a tweet's text
+def classify_text(text, model):
+	text_clean = [ cleanup_text(text) ]
+	prediction = model.predict(text_clean)
+	if prediction.iat[0, 2] == 'informative':
+		return(True)
+	else:
+		return(False)
+
+def classifyTweet(tweet, _, text_model, image_model):
+
+	# Initialize found flag
+	found = False
+
+	# DATA EXTRACTION
+	# Classify the tweet's text
+	text = tweet['data']['text']
+	if classify_text(text, text_model):
+		found = True
+
+	# TAGGING
+	# Surround words matching crawling rules with '&'
+	for rule in tweet['matching_rules']:
+		for word in rule['tag']:
+			text = text.replace(word, '&' + word + '&')
+
+	# CLASSIFICATION
+	# Classify the tweet's images
+	return(found, text)
---
+++ common/SMSTD.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# Praetorian H2020 Project
+# Work Package 6	:	Response Coordination
+# Task 4			:	Integration with Social Media
+# ~~~~~~~~~~~~~~~~~~~~
+# SMSTD-specific functionality
+
+import string
+
+tag_rules = 'lexicon_TD'
+tag_identifiers = 'identifiers_TD'
+tag_tweets = 'TD_tweets'
+a = 0
+b = 1
+
+# Classifies tweet as suspicious, returns boolean classification & annotated tweet text
+def classifyTweet(tweet, identifiers, _, __):
+
+	# Initialize found flag
+	found = False
+
+	# DATA EXTRACTION
+	# Get tweet text and location (if present)
+	text = tweet['data']['text']
+	location = (tweet['includes']['places'][0] if 'places' in tweet['includes'] else None)
+
+	# TAGGING
+	# Surround words matching crawling rules with '$'
+	for rule in tweet['matching_rules']:
+		for word in rule['tag']:
+			text = text.replace(word, '$' + word + '$')
+
+	# Clean up text and put it in a list
+	clean_text_list = text.casefold().translate(string.maketrans('', '', string.punctuation)).split()
+
+	# CLASSIFICATION
+	# Check text for identifiers, surround words matching identifiers with '&'
+	for identifier in identifiers:
+		if identifier['value'].casefold() in clean_text_list:
+			text = text.replace(identifier['value'], '&' + identifier['value'] + '&')
+			found = True
+
+	# Check location for identifiers
+	if place:
+		place_stringified = (' '.join(list(place.values()))).casefold()
+		if any(identifier['value'].casefold() in place_stringified for identifier in identifiers):
+			found = True
+
+	return(found, text)
