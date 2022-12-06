f48d8478f134f7038e3179a0ce34b48f5de11e33
UphillD <13258958+UphillD@users.noreply.github.com>
2022-12-06 18:26:00 +0200

    Remove unused IOP functions

--- common/iop.py
+++ common/iop.py
@@ -59,52 +59,6 @@ def get_identifiers(tag):
 	logger.success('{} CI identifiers received from IOP.'.format(len(identifiers)))
 	return(identifiers)
 
-# Gets queued tweets from the IOP
-def get_tweets():
-	# Initialize tweet list
-	tweets = []
-	logger.log('COMM', 'Quering the IOP for queued tweets.')
-	r = requests.get(config.urls['iop']['socialMedia'], params=config.query)
-	try:
-		r.raise_for_status()
-	except:
-		logger.error('Query failed (HTTP {}): {}.'.format(r.status_code, r.text))
-	else:
-		for element in r.json()['data']:
-			if 'collection' in element and element['collection'] == 'tweets2post':
-				logger.success('Tweet found: {}.'.format(element['_id']))
-				logger.info('Storing tweet locally')
-				tweets.append(element['text'])
-				logger.log('COMM', 'Quering the IOP to delete tweet.')
-				r = requests.delete(config.urls['iop']['socialMedia'] + element['_id'], params=config.query)
-				try:
-					r.raise_for_status
-				except:
-					logger.warn('Query failed (HTTP {}): {}.'.format(r.status_code, r.text))
-	return(tweets)
-
-# Initializes status flag in IOP
-# CURRENTLY NOT USED
-def init_status():
-	logger.log('COMM', 'Deleting flag from IOP.')
-	# Get running flag from IOP
-	r = requests.delete(config.urls['iop']['socialMedia'] + 'BkP9xH8s2dqgYuJKS', params=config.query)
-	try:
-		r.raise_for_status()
-	except Exception as e:
-		logger.error('Failed to communicate with IOP to delete status flag: {}'.format(str(e)))
-		return(False)
-	# Reset running flag
-	logger.log('COMM', 'Creating flag in IOP.')
-	payload = json.dumps({ '_id': 'BkP9xH8s2dqgYuJKS', 'value': False, 'collection': 'status' })
-	r = requests.post(config.urls['iop']['socialMedia'], params=config.query, data=payload, headers=config.headers)
-	try:
-		r.raise_for_status()
-	except Exception as e:
-		logger.error('Failed to communicate with IOP to create status flag: {}'.format(str(e)))
-		return(False)
-	return(True)
-
 # Gets status flag from IOP
 def get_status():
 	# Get running flag from IOP
