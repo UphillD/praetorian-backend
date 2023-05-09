5d404fa561479b842a16ccd06bd31efd590fbdeb
UphillD <13258958+UphillD@users.noreply.github.com>
2023-05-09 16:18:27 +0200

    Minor fix

--- common/winkwink.py
+++ common/winkwink.py
@@ -38,51 +38,9 @@ session.mount('https://', requests.adapters.HTTPAdapter(max_retries=retries))
 ###############
 ## FUNCTIONS ##
 ###############
-# Gets crawling rules from the IOP
-def get_rules(tag):
-	logger.log('COMM', 'Quering the IOP for crawling rules...')
-	r = session.get(config['URLs']['iop_socialMedia'], params=query_params, timeout=config['IOP'].getint('timeout'))
-	try:
-		r.raise_for_status()
-	except:
-		logger.error('Query failed (HTTP {}).'.format(r.status_code))
-		logger.error('Message: {}.'.format(r.text))
-		sys.exit(config['Exit Codes']['iop_get'])
-	# Generate list of rules
-	query_rules = []
-	for element in r.json()['data']:
-		if 'collection' in element and element['collection'] == tag:
-			query_rules.append({'value': element['value'], 'tag': element['tag']})
-	if not query_rules:
-		logger.error('No rules found in the IOP.')
-		sys.exit(config['Exit Codes'].getint('missing_rules'))
-	logger.success('{} rules received from IOP.'.format(len(query_rules)))
-	return(query_rules)
-
-# Gets CI identifiers from the IOP
-def get_identifiers(tag):
-	logger.log('COMM', 'Quering the IOP for CI identifiers...')
-	r = session.get(config['URLs']['iop_socialMedia'], params=query_params, timeout=config['IOP'].getint('timeout'))
-	try:
-		r.raise_for_status()
-	except:
-		logger.error('Query failed (HTTP {}).'.format(r.status_code))
-		logger.error('Message: {}.'.format(r.text))
-		sys.exit(config['Exit Codes']['iop_get'])
-	# Generate list of identifiers
-	identifiers = []
-	for element in r.json()['data']:
-		if 'collection' in element and element['collection'] == tag:
-			identifiers.append({'value': element['value'], 'type': element['type'], 'priority': element['priority']})
-	if not identifiers:
-		logger.error('No identifiers found in the IOP.')
-		sys.exit(config['Exit Codes'].getint('missing_identifiers'))
-	logger.success('{} CI identifiers received from IOP.'.format(len(identifiers)))
-	return(identifiers)
-
 # Registers tweet on the IOP
 def register_tweet(payload):
-	logger.log('COMM', 'Registering the tweet on the IOP...')
+	logger.info('Registering the tweet on the IOP...')
 	while True:
 		r = session.post(config['URLs']['iop_socialMedia'], data=payload, params=query_params, headers=headers, timeout=config['IOP'].getint('timeout'))
 		try:
@@ -90,21 +48,10 @@ def register_tweet(payload):
 		except:
 			logger.error('Failed to register tweet on IOP (HTTP {}).'.format(r.status_code))
 			logger.error('Message: {}.'.format(r.text))
-			logger.log('COMM', 'Retrying...')
 		else:
 			logger.success('Tweet registered on IOP.')
 			return(True)
 
-# Gets status flag from IOP
-def get_status(previous_status):
-	r = session.get(config['URLs']['iop_socialMedia'] + config['IOP']['key'], params=query_params, timeout=config['IOP'].getint('timeout'))
-	try:
-		r.raise_for_status()
-	except:
-		logger.error('Failed to get status flag from IOP (HTTP {}).'.format(r.status_code))
-		logger.error('Message: {}.'.format(r.text))
-		return(previous_status)
-	return(r.json()['data']['value'])
 
 def generate_tweet():
 	first_tweet = json.dumps({
